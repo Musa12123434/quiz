4f3add92591fdd0ad30768f5278eb3ef44e56c44
user <usernameqwert3@gmail.com>
2026-06-11 02:38:37 +0400

    changes in js and html

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let correctAnswers = document.querySelectorAll('input');
+let correctAnswers = document.querySelectorAll('.correct');
 let button = document.querySelector('button');
 let result = document.querySelector('.result');
-let numberofquestions = correctanswercount.length;
+let numberofquestions = correctAnswers.length;
 button.addEventListener('click', () => {
+    console.log(correctAnswers)
     let correctanswercount = 0;
     for (let i = 0; i < correctAnswers.length; i++) {
         if (correctAnswers[i].checked) { correctanswercount += 1; }
